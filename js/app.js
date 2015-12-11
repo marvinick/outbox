3c28ba12a8087f85bf04bee411b7623c7846b1eb
marvin <marvkipi@gmail.com>
2015-12-10 23:14:39 -0800

    routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 var app = angular.module("OutboxApp", []);
+app.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'HomeController',
+    templateUrl: 'views/home.html'
+  })
+  .when('/outbox/:id', {
+    controller: 'EmailController',
+    templateUrl: 'views/email.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
--- js/services/emails.js
+++ js/services/emails.js
@@ -1,20 +1,9 @@
 app.factory('emails', ['$http', function($http) {
   return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json')
-        .success(function(data) {
-           return data;
-         })
-        .error(function(err) { 
-           return err; 
-         }); 
-}]);
-
-
-app.factory('forecast', ['$http', function($http) { 
-  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json') 
-            .success(function(data) { 
-              return data; 
-            }) 
-            .error(function(err) { 
-              return err; 
-            }); 
+            .success(function(data) {
+              return data;
+            })
+            .error(function(err) {
+              return err;
+            });
 }]);
